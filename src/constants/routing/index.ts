7e3fcc453da1677e90ee50c9d81b26f5034fb949
Denys Karakuta <den456988@gmail.com>
2024-01-26 16:14:29 +0200

    feat: add routing constants

---
+++ src/constants/routing/index.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    ISSUES: '/issues',
+    DASHBOARD: '/',
+    DEFAULT: '/',
+};
+
+export const NAVIGATION = [
+    { label: 'Dashboard', href: ROUTES.DASHBOARD },
+    { label: 'Issues', href: ROUTES.ISSUES },
+];
