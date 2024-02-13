600ef98c38c2edc50b62fda7e4d8d55adac48b5f
Denys Karakuta <den456988@gmail.com>
2024-02-13 17:11:35 +0200

    feat: add theme-config.css to make correct font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,14 @@ import type { Metadata } from 'next';
 import Navbar from '@/src/components/common/Navbar';
 
 import '@radix-ui/themes/styles.css';
+import '@/src/assets/styles/theme-config.css';
 import '@/src/assets/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
     title: 'Issues tracker',
@@ -17,10 +22,10 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <Theme>
+            <body className={inter.variable}>
+                <Theme appearance="light" accentColor="ruby">
                     <Navbar />
-                    {children}
+                    <main className="p-5">{children}</main>
                 </Theme>
             </body>
         </html>
