812a810739914f3b82202ca847e3cc4ad7c2fd38
Denys Karakuta <den456988@gmail.com>
2024-04-16 10:16:14 +0300

    feat: add issues Table display

--- src/app/(pages)/issues/page.tsx
+++ src/app/(pages)/issues/page.tsx
@@ -1,15 +1,46 @@
 import Link from 'next/link';
 
-import { Button } from '@radix-ui/themes';
+import { Button, Table } from '@radix-ui/themes';
+
+import { fetchManyIssues } from '@/services/prisma/issues';
 
 import { ROUTES } from '@/constants/routing';
 
-const IssuesPage: React.FC = () => {
+const IssuesPage: React.FC = async () => {
+    const issues = await fetchManyIssues();
+
+    const renderIssues = issues.map((issue) => (
+        <Table.Row key={issue.id}>
+            <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+            </Table.Cell>
+
+            <Table.Cell className="hidden md:table-cell">{issue.status}</Table.Cell>
+
+            <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+        </Table.Row>
+    ));
+
     return (
         <div>
-            <Button size="3">
+            <Button size="3" className="mb-5">
                 <Link href={ROUTES.ISSUES_NEW}>New Issue</Link>
             </Button>
+
+            <Table.Root variant="surface">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
+
+                        <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>{renderIssues}</Table.Body>
+            </Table.Root>
         </div>
     );
 };
