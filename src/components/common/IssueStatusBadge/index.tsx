1988899a0cd1d6c33243a1351361566b312234d8
Denys Karakuta <den456988@gmail.com>
2024-04-17 11:38:23 +0300

    refactor: change name to ISSUE_STATUS

--- src/components/common/IssueStatusBadge/index.tsx
+++ src/components/common/IssueStatusBadge/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Status } from '@prisma/client';
 import { Badge } from '@radix-ui/themes';
 
-import { STATUS_MAP } from '@/constants/issue';
+import { ISSUE_STATUS } from '@/constants/issue';
 
 type OwnProps = {
     status: Status;
@@ -11,7 +11,7 @@ type OwnProps = {
 const IssueStatusBadge: React.FC<OwnProps> = (props) => {
     const { status } = props;
 
-    const { label, color } = STATUS_MAP[status];
+    const { label, color } = ISSUE_STATUS[status];
 
     return <Badge color={color}>{label}</Badge>;
 };
--- src/constants/issue/index.ts
+++ src/constants/issue/index.ts
@@ -1,4 +1,4 @@
-export const STATUS_MAP = {
+export const ISSUE_STATUS = {
     IN_PROGRESS: { label: 'In Progress', color: 'violet' },
     CLOSED: { label: 'Closed', color: 'green' },
     OPEN: { label: 'Open', color: 'red' },
