21107118ce2e8528ff2e326a05605387614b7dd6
Denys Karakuta <den456988@gmail.com>
2024-04-18 11:50:56 +0300

    feat: add prisma service fetchIssueById

--- src/services/prisma/issues/index.ts
+++ src/services/prisma/issues/index.ts
@@ -1,3 +1,10 @@
 import prisma from '@/configs/prisma/client';
 
 export const fetchManyIssues = () => prisma.issue.findMany();
+
+export const fetchIssueById = (id: string) =>
+    prisma.issue.findUnique({
+        where: {
+            id: parseInt(id),
+        },
+    });
