730f3dda5a582b95de8d1d419281e4b0107dedaf
Denys Karakuta <den456988@gmail.com>
2024-04-18 12:33:24 +0300

    feat: add Skeleton to LoadingIssueDetailPage

--- src/app/(pages)/issues/[id]/loading.tsx
+++ src/app/(pages)/issues/[id]/loading.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Box, Flex, Card } from '@radix-ui/themes';
 
-import Spinner from '@/components/common/Spinner';
+import Skeleton from '@/components/common/Skeleton';
 
-const LoadingIssueDetailPage = () => {
-    return <Spinner />;
+const LoadingIssueDetailPage: React.FC = () => {
+    return (
+        <Box className="max-w-xl">
+            <Skeleton />
+
+            <Flex className="space-x-3" my="2">
+                <Skeleton width="5rem" />
+                <Skeleton width="8rem" />
+            </Flex>
+
+            <Card className="prose" mt="4">
+                <Skeleton count={3} />
+            </Card>
+        </Box>
+    );
 };
 
 export default LoadingIssueDetailPage;
