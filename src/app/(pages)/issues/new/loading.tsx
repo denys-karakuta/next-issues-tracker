fddefc5ef485be08c4eafbdfd31569fcc3a8b4dc
Denys Karakuta <den456988@gmail.com>
2024-04-18 11:50:07 +0300

    feat: add LoadingNewIssuePage

---
+++ src/app/(pages)/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Spinner from '@/components/common/Spinner';
+
+const LoadingNewIssuePage = () => {
+    return <Spinner />;
+};
+
+export default LoadingNewIssuePage;
