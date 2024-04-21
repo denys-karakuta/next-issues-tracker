b42422f87c4e7c0671a8eb0d22634dce50ecac55
Denys Karakuta <den456988@gmail.com>
2024-04-21 11:15:00 +0300

    feat: add EditIssueButton to IssueDetailPage

--- src/app/(pages)/issues/[id]/page.tsx
+++ src/app/(pages)/issues/[id]/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
-import { Heading, Flex, Card, Text } from '@radix-ui/themes';
+import { Heading, Flex, Card, Text, Grid, Box } from '@radix-ui/themes';
 
 import IssueStatusBadge from '@/components/common/IssueStatusBadge';
 
 import { fetchIssueById } from '@/services/prisma/issues';
+
+import EditIssueButton from './_components/EditIssueButton';
 
 type OwnProps = {
     params: { id: string };
@@ -21,19 +23,25 @@ const IssueDetailPage: React.FC<OwnProps> = async (props) => {
     }
 
     return (
-        <>
-            <Heading>{issue.title}</Heading>
+        <Grid columns={{ initial: '1', sm: '5' }} gap="5">
+            <Box>
+                <Heading>{issue.title}</Heading>
 
-            <Flex className="space-x-3" my="2">
-                <IssueStatusBadge status={issue.status} />
+                <Flex className="space-x-3" my="2">
+                    <IssueStatusBadge status={issue.status} />
 
-                <Text>{issue.createdAt.toDateString()}</Text>
-            </Flex>
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                </Flex>
 
-            <Card className="prose max-w-full" mt="4">
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-        </>
+                <Card className="prose max-w-full" mt="4">
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+
+            <Box>
+                <EditIssueButton issueId={issue.id} />
+            </Box>
+        </Grid>
     );
 };
 
