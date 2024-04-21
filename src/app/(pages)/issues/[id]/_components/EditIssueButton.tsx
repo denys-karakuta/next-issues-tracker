a76b60c3002a652ac81c41edf6e562b6712bbafc
Denys Karakuta <den456988@gmail.com>
2024-04-21 11:14:40 +0300

    feat: add EditIssueButton component

---
+++ src/app/(pages)/issues/[id]/_components/EditIssueButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from '@radix-ui/themes';
+import { HiOutlinePencilSquare } from 'react-icons/hi2';
+
+import { ROUTES } from '@/constants/routing';
+
+type OwnProps = { issueId: number };
+
+const EditIssueButton: React.FC<OwnProps> = (props) => {
+    const { issueId } = props;
+
+    return (
+        <Button size="3">
+            <HiOutlinePencilSquare size="20" />
+
+            <Link href={`${ROUTES.ISSUES_EDIT}/${issueId}`}>Edit Issue</Link>
+        </Button>
+    );
+};
+
+export default EditIssueButton;
