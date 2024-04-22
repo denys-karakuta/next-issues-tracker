1f1e8c900b65423f324500f37c299eed83982185
Denys Karakuta <den456988@gmail.com>
2024-04-22 13:14:24 +0300

    refactor: service prisma issues

--- src/services/prisma/issues/index.ts
+++ src/services/prisma/issues/index.ts
@@ -1,18 +1,22 @@
 import prisma from '@/configs/prisma/client';
 
-export const fetchIssuesList = () => prisma.issue.findMany();
+export const fetchIssuesList = () => {
+    return prisma.issue.findMany();
+};
 
-export const fetchIssueById = (id: string) =>
-    prisma.issue.findUnique({
+export const fetchIssueById = (id: string) => {
+    return prisma.issue.findUnique({
         where: {
             id: parseInt(id),
         },
     });
+};
 
-export const updateIssueByIdRequest = (id: string, data) =>
-    prisma.issue.update({
+export const updateIssueByIdRequest = (id: string, data) => {
+    return prisma.issue.update({
         where: {
             id: parseInt(id),
         },
         data,
     });
+};
