bac97df0298d2c0dd25b3de2a2e4d6197fc507e3
Denys Karakuta <den456988@gmail.com>
2024-04-24 11:16:22 +0300

    feat: add deleteIssueRequest api

--- src/services/api/issues/index.ts
+++ src/services/api/issues/index.ts
@@ -9,3 +9,7 @@ export const createIssueRequest = async ({ requestBody }, config?: AxiosRequestC
 export const updateIssueRequest = async ({ id, requestBody }, config?: AxiosRequestConfig) => {
     return await api.patch(`v1/issues/${id}`, requestBody, config);
 };
+
+export const deleteIssueRequest = async ({ id }, config?: AxiosRequestConfig) => {
+    return await api.delete(`v1/issues/${id}`, config);
+};
