1a0ebb07044208246191626631d2117e6aa646bd
Denys Karakuta <den456988@gmail.com>
2024-05-01 13:52:59 +0300

    feat: render users in AssigneeSelect

--- src/app/(pages)/issues/view/[id]/_components/AssigneeSelect.tsx
+++ src/app/(pages)/issues/view/[id]/_components/AssigneeSelect.tsx
@@ -1,16 +1,32 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 
+import useUsersActions from '@/hooks/useUsersActions';
+
 const AssigneeSelect: React.FC = () => {
+    const { getUsersAction } = useUsersActions();
+    const [users, setUsers] = useState<User>([]);
+
+    useEffect(() => {
+        getUsersAction().then((data) => setUsers(data));
+    }, []);
+
+    const renderUsers = users?.map((user) => (
+        <Select.Item key={user.id} value={user.id}>
+            {user.name}
+        </Select.Item>
+    ));
+
     return (
         <Select.Root size="3">
             <Select.Trigger placeholder="Assign..." />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="1">User</Select.Item>
+                    {renderUsers}
                 </Select.Group>
             </Select.Content>
         </Select.Root>
