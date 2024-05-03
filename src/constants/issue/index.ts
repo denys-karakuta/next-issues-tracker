936bc3d4caec35aee3f43370ff211cb56be52c73
Denys Karakuta <den456988@gmail.com>
2024-05-03 19:19:43 +0300

    feat: add ISSUES_FILTER_OPTIONS

--- src/constants/issue/index.ts
+++ src/constants/issue/index.ts
@@ -1,5 +1,16 @@
+import { Status } from '@prisma/client';
+
 export const ISSUE_STATUS = {
     IN_PROGRESS: { label: 'In Progress', color: 'violet' },
     CLOSED: { label: 'Closed', color: 'green' },
     OPEN: { label: 'Open', color: 'red' },
 } as const;
+
+type IssuesFilterStatus = { label: string; value?: Status | 'ALL' }[];
+
+export const ISSUES_FILTER_OPTIONS: IssuesFilterStatus = [
+    { label: 'All', value: 'ALL' },
+    { label: 'Open', value: 'OPEN' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
+    { label: 'Closed', value: 'CLOSED' },
+];
