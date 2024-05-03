26a14ee008ccbfc7e37b5c851f3742be4822e4e8
Denys Karakuta <den456988@gmail.com>
2024-05-03 19:20:35 +0300

    feat: add IssuesStatusFilter in IssueActions

--- src/app/(pages)/issues/list/_components/IssueActions.tsx
+++ src/app/(pages)/issues/list/_components/IssueActions.tsx
@@ -4,12 +4,16 @@ import { Button, Flex } from '@radix-ui/themes';
 
 import { ROUTES } from '@/constants/routing';
 
+import IssuesStatusFilter from './IssuesStatusFilter';
+
 const IssueActions = () => {
     return (
         <Flex justify="between" className="mb-5">
             <Button size="3">
                 <Link href={`/${ROUTES.ISSUES_CREATE}`}>New Issue</Link>
             </Button>
+
+            <IssuesStatusFilter />
         </Flex>
     );
 };
