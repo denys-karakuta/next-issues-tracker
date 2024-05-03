f918fd33ac4d6b540db8ee63993fd29c08ff6aae
Denys Karakuta <den456988@gmail.com>
2024-05-03 13:15:03 +0300

    feat: add patchIssueSchema

--- src/schemas/issues/index.ts
+++ src/schemas/issues/index.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 
 export const issueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1),
+    title: z.string().min(1, 'title is required.').max(255),
+    description: z.string().min(1, 'description is required.').max(65535),
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, 'title is required.').max(255).optional(),
+    description: z.string().min(1, 'description is required.').max(65535).optional(),
+    assignedToUserId: z.string().min(1, 'assignedToUserId is required.').max(255).optional().nullable(),
 });
