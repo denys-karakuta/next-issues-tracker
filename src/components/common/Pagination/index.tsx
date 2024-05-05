490dedc1af6f63b6cb91a0709f1cb1019b312456
Denys Karakuta <den456988@gmail.com>
2024-05-05 08:37:05 +0300

    feat: add Pagination common component

---
+++ src/components/common/Pagination/index.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import React from 'react';
+import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons';
+
+type OwnProps = {
+    currentPage: number;
+    itemsCount: number;
+    pageSize: number;
+};
+
+const Pagination: React.FC<OwnProps> = (props) => {
+    const { itemsCount, pageSize, currentPage } = props;
+
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const pageCount = Math.ceil(itemsCount / pageSize);
+
+    if (pageCount <= 1) {
+        return null;
+    }
+
+    const changePage = (page: number) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', page.toString());
+        router.push('?' + params.toString());
+    };
+
+    return (
+        <Flex align="center" gap="2">
+            <Text size="2">
+                Page {currentPage} of {pageCount}
+            </Text>
+
+            <Button color="gray" variant="soft" disabled={currentPage === 1} onClick={() => changePage(1)}>
+                <DoubleArrowLeftIcon />
+            </Button>
+
+            <Button color="gray" variant="soft" disabled={currentPage === 1} onClick={() => changePage(currentPage - 1)}>
+                <ChevronLeftIcon />
+            </Button>
+
+            <Button color="gray" variant="soft" disabled={currentPage === pageCount} onClick={() => changePage(currentPage + 1)}>
+                <ChevronRightIcon />
+            </Button>
+
+            <Button color="gray" variant="soft" disabled={currentPage === pageCount} onClick={() => changePage(pageCount)}>
+                <DoubleArrowRightIcon />
+            </Button>
+        </Flex>
+    );
+};
+
+export default Pagination;
