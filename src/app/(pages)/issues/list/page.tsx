f4711309e58395a36886901daf24ae60d0d5ff1b
Denys Karakuta <den456988@gmail.com>
2024-05-05 07:55:03 +0300

    feat: make columns sortable

--- src/app/(pages)/issues/list/page.tsx
+++ src/app/(pages)/issues/list/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import NextLink from 'next/link';
 import { Status } from '@prisma/client';
 import { Table } from '@radix-ui/themes';
+import { LuArrowUpAZ } from 'react-icons/lu';
 
 import IssueStatusBadge from '@/components/common/IssueStatusBadge';
 import Link from '@/components/common/Link';
@@ -11,14 +13,43 @@ import { ROUTES } from '@/constants/routing';
 
 import IssueActions from './_components/IssueActions';
 
+const columns = [
+    { label: 'Issue', value: 'title' },
+    {
+        label: 'Status',
+        value: 'status',
+        className: 'hidden md:table-cell',
+    },
+    {
+        label: 'Created',
+        value: 'createdAt',
+        className: 'hidden md:table-cell',
+    },
+] as const;
+
 type OwnProps = {
-    searchParams: { status: Status };
+    searchParams: { status: Status; orderBy: 'status' | 'title' | 'createdAt' };
 };
 
 const IssuesPage: React.FC<OwnProps> = async (props) => {
     const { searchParams } = props;
 
-    const issues = await fetchIssuesList({ where: { status: searchParams.status || undefined } });
+    const status = Object.values(Status).includes(searchParams.status) ? searchParams.status : undefined;
+    const orderBy = columns.map((column) => column.value).includes(searchParams.orderBy) ? { [searchParams.orderBy]: 'asc' } : undefined;
+
+    const issues = await fetchIssuesList({ where: { status }, orderBy });
+
+    const renderColumns = (
+        <Table.Row>
+            {columns.map((column) => (
+                <Table.ColumnHeaderCell key={column.value}>
+                    <NextLink href={{ query: { ...searchParams, orderBy: column.value } }}>{column.label}</NextLink>
+
+                    {column.value === searchParams.orderBy ? <LuArrowUpAZ className="ml-1 size-5 inline" /> : null}
+                </Table.ColumnHeaderCell>
+            ))}
+        </Table.Row>
+    );
 
     const renderIssues = issues.map((issue) => (
         <Table.Row key={issue.id}>
@@ -43,15 +74,7 @@ const IssuesPage: React.FC<OwnProps> = async (props) => {
             <IssueActions />
 
             <Table.Root variant="surface">
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
+                <Table.Header>{renderColumns}</Table.Header>
 
                 <Table.Body>{renderIssues}</Table.Body>
             </Table.Root>
